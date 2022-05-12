19ab64f67285b8edc415dfe380968fc856a721fa
John Doe <leehy0782@naver.com>
2022-05-12 21:46:51 +0900

    feat: postPage - img upload, preview etc...

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,27 +1,9 @@
 import React from "react";
 import styled from "styled-components";
-import RightSide from "../components/mainPost/RightSide";
-import LeftSide from "../components/mainPost/LeftSide";
 import Mainpost from "../components/mainPost/MainPost";
-
-const Main = styled.div`
-  font-family: Noto Sans KR;
-  position: relative;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  width: 100vw;
-  height: 100vh;
-  background-color: #EFF4EF;
-  box-sizing: border-box;
-`
 
 export default function MainPage() {
   return (
-    <Main>
-      <LeftSide />
       <Mainpost postId={""} author={""} title={""} content={""} imgUrl={[]} like={0} comment={[]} />
-      <RightSide/>
-    </Main>
   );
 }
